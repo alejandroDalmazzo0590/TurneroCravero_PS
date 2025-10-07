8701f87d3ec9ca52533e4a816b765adeb6b9b425
Alejandro Dalmazzo <alejandro.dalmazzo@gmail.com>
2025-10-06 23:15:39 -0300

    add redirects for RestDB proxy

--- src/api/apiAxios.js
+++ src/api/apiAxios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiAxios = axios.create({
-  baseURL: "https://turnoscravero-7d0e.restdb.io/rest",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
     "x-apikey": "9bcb8157f9e319253d13016c32a3273b2cb40"
